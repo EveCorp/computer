b153cabae089d87566acb278d6859ee50840dc3a
vanitasvitae <vanitasvitae@fsfe.org>
2017-02-27 00:11:36 +0100

    Added first fork if-else

--- src/computer/main.cpp
+++ src/computer/main.cpp
@@ -2,6 +2,14 @@
 
 int main(int argc, char *argv[])
 {
+    pid_t pid = fork();
+
+	if (pid < 0) {
+		return EXIT_FAILURE;
+	}
+	if (pid > 0) {
+		//Setup
+	}
 	std::cout << "Hello World!" << std::endl;
-	return 0;
+	return EXIT_SUCCESS;
 }
